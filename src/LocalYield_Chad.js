cb04fbd39d4039b50c15dc88516facb2dc583e8a
J. Benjamin Cook <jbenjamincook@gmail.com>
2014-04-09 16:58:16 -0400

    setting up time series

--- src/LocalYield_Chad.js
+++ src/LocalYield_Chad.js
@@ -123,20 +123,25 @@ yieldMeta.append("rect")
     .attr("width", bbYieldMeta.w)
     .attr("height", bbYieldMeta.h)
 
+var parseDate = d3.time.format("%d-%b-%y").parse;
 
 // Load data and create visualizations
 queue()
     .defer(d3.json, "../data/nebraska.geojson")
     .defer(d3.csv, "../data/wmk5_2009_small.csv")
+    .defer(d3.csv, "../data/price.csv")
     .await(createVis)
 
-function createVis(error, geo_data, yield_data) {
+
+function createVis(error, geo_data, yield_data, price) {
 
     createYieldMeta();
 
     var x = path.centroid(geo_data.features[2]);
 
     projection.translate([bbFieldVis.w - x[0], bbFieldVis.h - x[1]]);
+
+    timeSeries(price);
 
     point = fieldVis.selectAll(".point")
         .data(yield_data)
@@ -305,20 +310,21 @@ function highlightBrushedYield(){
 
 }
 
-var brushField = d3.svg.brush()
-    .x(d3.scale.linear().domain([0, 100]).range([0, bbFieldVis.w]))
-    .y(d3.scale.identity().domain([0, bbFieldVis.h]))
+// var brushField = d3.svg.brush()
+//     .x(d3.scale.linear().domain([0, 100]).range([0, bbFieldVis.w]))
+//     .y(d3.scale.identity().domain([0, bbFieldVis.h]))
 
-    .on("brush", brushedField);
+//     .on("brush", brushedField);
 
 fieldVis.append("g")
     .attr("class", "brush")
-    .call(brushField)
+//    .call(brushField)
     .selectAll("rect")
     .attr("height", bbFieldVis.h);
 
 function brushedField() {
 }
+
 
 
 var showYield = function(d) {
@@ -417,4 +423,62 @@ function createYieldMeta() {
         .style("fill", "grey")
         .style("font-weight", "bold")
         .style("font-size", 16);                    
+}
+
+
+var timeSeries = function(data) {
+    console.log(data);
+//     var x = d3.time.scale()
+//     .range([0, width]);
+
+//     var y = d3.scale.linear()
+//         .range([height, 0]);
+
+//     var xAxis = d3.svg.axis()
+//         .scale(x)
+//         .orient("bottom");
+
+//     var yAxis = d3.svg.axis()
+//         .scale(y)
+//         .orient("left");
+
+//     var line = d3.svg.line()
+//         .x(function(d) { return x(d.date); })
+//         .y(function(d) { return y(d.close); });
+
+//     var svg = d3.select("body").append("svg")
+//         .attr("width", width + margin.left + margin.right)
+//         .attr("height", height + margin.top + margin.bottom)
+//       .append("g")
+//         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+//     d3.tsv("data.tsv", function(error, data) {
+//       data.forEach(function(d) {
+//         d.date = parseDate(d.date);
+//         d.close = +d.close;
+//       });
+
+//       x.domain(d3.extent(data, function(d) { return d.date; }));
+//       y.domain(d3.extent(data, function(d) { return d.close; }));
+
+//       svg.append("g")
+//           .attr("class", "x axis")
+//           .attr("transform", "translate(0," + height + ")")
+//           .call(xAxis);
+
+//       svg.append("g")
+//           .attr("class", "y axis")
+//           .call(yAxis)
+//         .append("text")
+//           .attr("transform", "rotate(-90)")
+//           .attr("y", 6)
+//           .attr("dy", ".71em")
+//           .style("text-anchor", "end")
+//           .text("Price ($)");
+
+//       svg.append("path")
+//           .datum(data)
+//           .attr("class", "line")
+//           .attr("d", line);
+// });
 }
